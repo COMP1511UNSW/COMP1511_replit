5864a9e542845a3f3e16fa510a1c4656050afbf1
Tom Kunc <tomkunc0@gmail.com>
2021-08-11 11:50:31 +1000

    Add files for replit

---
+++ main.c
@@ -0,0 +1,18 @@
+// Name of Exercise
+// name.c
+//
+// This program was written by YOUR-NAME-HERE (z5555555)
+// on INSERT-DATE-HERE
+//
+// One line summary of what this exercise does.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void) {
+
+    // TODO: complete something here.
+
+    return 0;
+}
